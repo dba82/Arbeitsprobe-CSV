f8893d6750cf43c0b9476be2ddcd281ff05782fb
dba82 <davidhbassler@gmail.com>
2022-09-25 15:19:18 +0200

    Kommentare hinzugefügt

--- src/app/Components/chart-table/chart-table.component.ts
+++ src/app/Components/chart-table/chart-table.component.ts
@@ -1,6 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { ChartData } from '../../Data Classes/chart-data';
-
+/**
+ * Der ChartTable stellt die relative und absolute Häufigkeit
+ * der Items im ChartData Objekt tabellarisch dar, die Hintergrundfarben
+ * werden im CSS definiert und sollten denen im begleitenden PieChartComponent
+ * entsprechen.
+ */
 @Component({
   selector: 'app-chart-table',
   templateUrl: './chart-table.component.html',
